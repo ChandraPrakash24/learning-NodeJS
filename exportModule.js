f27e5d3599b49537f2fbea8534d7edc22dcc0825
ChandraPrakash24 <2412prakas@gmail.com>
2023-12-10 11:21:34 +0530

    learning import export module

---
+++ exportModule.js
@@ -0,0 +1,21 @@
+// COMMONJS WAY : use exports with s not only export
+
+exports.sum = (a,b) => {
+    return a + b;
+}
+
+exports.diff = (a,b) => {
+    return b - a;
+}
+
+// EJS WAY
+
+// const sum = (a,b) => {
+//     return a + b;
+// }
+
+// const diff = (a,b) => {
+//     return b - a;
+// }
+
+// export {sum,diff};
---
+++ importModule.js
@@ -0,0 +1,14 @@
+// COMMONJS WAY
+
+
+const maths = require('./exportModule.js')
+
+console.log(maths.sum(1,2));
+console.log(maths.diff(5,1));
+
+// EJS WAY
+
+// import {sum,diff} from './exportModule.js';
+
+// console.log(sum(3,3));
+// console.log(diff(1,5));
