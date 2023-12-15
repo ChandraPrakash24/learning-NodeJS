ad31b1089d7909b4e4932ce9503ac3f7190c6a56
ChandraPrakash24 <2412prakas@gmail.com>
2023-12-15 14:23:34 +0530

    Learning Routes under mvc file structure

--- ch5 MVC and FS Structure/index.js
+++ ch5 MVC and FS Structure/index.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const app = express();
 const productController = require('./controller/product.js');
+const productRouter = require('./routes/productRouts.js');
 
 app.use(express.json());  //body-parser
-
-app.post('/products', productController.postProducts);
-app.get('/products', productController.getProduct)
-app.get('/products/:id', productController.getSpecificProduct)
-app.put('/products/:id', productController.putSpecificProduct)
-app.patch('/products/:id', productController.patchSpecificProduct);
-app.delete('/products/:id', productController.deleteSpecificProduct)
+app.use('/products',productRouter.routes);
 
 app.listen(3000, () => { console.log('server started'); });
---
+++ ch5 MVC and FS Structure/routes/productRouts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const productController = require('../controller/product.js');
+const routes = express.Router();
+
+routes
+    .post('/', productController.postProducts)
+    .get('/', productController.getProduct)
+    .get('/:id', productController.getSpecificProduct)
+    .put('/:id', productController.putSpecificProduct)
+    .patch('/:id', productController.patchSpecificProduct)
+    .delete('/:id', productController.deleteSpecificProduct)
+
+exports.routes = routes;
